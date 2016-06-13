19ee4def1d17af0a562715166bc8e6c28c218b7e
Dayne Johnston <lowtekn7@gmail.com>
2016-06-13 19:36:35 -0400

    Added missing files.

---
+++ InitiativeTracker.WebUI/Controllers/CharacterController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using InitiativeTracker.Domain.Abstract;
+using InitiativeTracker.Domain.Entities;
+using InitiativeTracker.Domain.Concrete;
+using InitiativeTracker.WebUI.Infrastructure;
+
+using InitiativeTracker.WebUI.Models;
+
+namespace InitiativeTracker.WebUI.Controllers.REST
+{
+    public class CharacterController : Controller
+    {
+        private ICharacterRepository characters;
+        private ICharacterGroupRepository groups;
+        
+        public CharacterController(ICharacterGroupRepository groupRepo, ICharacterRepository characterRepo)
+        {
+            this.characters = characterRepo;
+            this.groups = groupRepo;
+        }
+
+        public ViewResult Summary()
+        {
+            IEnumerable<CharacterViewModel> list = SharedMethods.ConvertIEToCharacterVM(
+                characters.items, groups.items);
+            return View(list);
+        }
+
+        public ViewResult Create()
+        {
+            CreateCharacterViewModel vm = new CreateCharacterViewModel();
+            vm.GetGroups = SharedMethods.SelectListOf(groups);
+            return View(vm);
+        }
+
+        public ActionResult Cancel()
+        {
+            return RedirectToAction("Summary");
+        }
+
+        public ViewResult Edit(int id)
+        {
+            Character item = characters.Get(id);
+
+            CreateCharacterViewModel vm = new CreateCharacterViewModel()
+            {
+                Character_ID = item.Character_ID,
+                Name = item.Name,
+                Initiative_Bonus = item.Initiative_Bonus,
+                Group_ID = item.Group_ID
+            };
+
+            vm.GetGroups = SharedMethods.SelectListOf(groups);
+            return View(vm);
+        }
+
+        public ActionResult Save(CreateCharacterViewModel item)
+        {
+            Character character = new Character()
+            {
+                Name = item.Name,
+                Character_ID = item.Character_ID,
+                Group_ID = item.Group_ID,
+                Initiative = item.Initiative,
+                Initiative_Bonus = item.Initiative_Bonus
+            };
+            characters.Save(character);
+            return RedirectToAction("Summary");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            characters.Remove(id);
+            return RedirectToAction("Summary");
+        }
+    }
+}
---
+++ InitiativeTracker.WebUI/Models/EncounterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using InitiativeTracker.Domain.Entities;
+using InitiativeTracker.WebUI.Models;
+
+namespace InitiativeTracker.WebUI.Models
+{
+    public class EncounterViewModel : Encounter
+    {
+        public IEnumerable<EncounterCharacterViewModel> EncounterCharacters { get; set; }
+        public int Round { get; set; }
+    }
+}
